464a5762a662827f0fabd22cf9e6b039866ba454
Aakash Chhabra <32056016+aakashchhabra1989@users.noreply.github.com>
2023-12-13 14:05:17 +0530

    Update notesUpdate.java

--- src/main/java/notesUpdate.java
+++ src/main/java/notesUpdate.java
@@ -1,19 +1,3 @@
-/**
- * Copyright 2010-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * <p>
- * This file is licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License. A copy of
- * the License is located at
- * <p>
- * http://aws.amazon.com/apache2.0/
- * <p>
- * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
-
-package dev.labs.dynamodb;
-
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
