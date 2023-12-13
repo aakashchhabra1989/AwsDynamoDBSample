1f0a17746c28a749ccb129cbb94ff0866f265372
Aakash Chhabra <32056016+aakashchhabra1989@users.noreply.github.com>
2023-12-13 14:04:20 +0530

    Update notesCRUDmapper.java

--- src/main/java/notesCRUDmapper.java
+++ src/main/java/notesCRUDmapper.java
@@ -1,5 +1,3 @@
-package dev.labs.dynamodb;
-
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.*;
